b08b93ffcb7bae3830fb6eb3ca6b4f8d01551690
Marek Štefanko <marcostesvk@gmail.com>
2021-08-08 13:51:52 +0300

    Initial commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { DashboardLayout } from "../components/Layout";
+
+const HomePage = () => {
+  return (
+    <DashboardLayout>
+      <div class="aspect-w-16 aspect-h-9"></div>
+    </DashboardLayout>
+  );
+};
+
+export default HomePage;
